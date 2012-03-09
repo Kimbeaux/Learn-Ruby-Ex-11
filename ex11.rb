1ba79f2c7e225165b95b424dd6e4c2081db9bc63
Kimbeaux <KimbeauxE@verizon.net>
2012-03-09 17:33:34 -0500

    ex 11 w/ ec

--- ex11.rb
+++ ex11.rb
@@ -6,3 +6,22 @@ print "How much do you weigh? "
 weight = gets.chomp()
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+# gets retrieves strings from the keyboard w/ the '\n' character.  chomp removes the '\n'.
+
+print "Enter your name: "
+name = gets.chomp
+puts "Hello #{name}"
+
+# This section of code hangs.
+# print "Enter your some more names separated by commas: "
+# while true
+#  name = gets(',').chomp(',')
+#  puts "Hello #{name}"
+# end
+
+print "What city are you from? "
+city = gets.chomp()
+print "Where do you live now? "
+new_city = gets.chomp()
+puts "Hope you like #{new_city} better than #{city}."
